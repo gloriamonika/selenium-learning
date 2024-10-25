947cec0245d583d448d4e5802f5f0d90024c8968
Gloria Monika <gloria.monika@gmail.com>
2024-10-25 15:11:38 +0700

    section 2.14-15

--- demo/src/main/java/selenium/exercise/Main.java
+++ demo/src/main/java/selenium/exercise/Main.java
@@ -9,11 +9,25 @@ public class Main {
 
         //invoke browser manually, results in faster execution and more specific version of the Chrome (maybe needed to support older releases)
         //System.setProperty("webdriver.chrome.drive", "path to chromedriver.exe");
+        
+        //invoke Firefox via GeckoDriver
+        //System.setProperty("webdriver.gecko.driver", "path to geckodriver.exe");
 
+        //invoke Edge via EdgeDriver
+        // System.setProperty("webdriver.edge.driver", "path to edgedriver");
+        
         //invoke browser via selenium manager
         WebDriver driver = new ChromeDriver(); //-> more generic but gives more flexibility to adapt the code for cross browser compabilities
+        // WebDriver driver = new FirefoxDriver();
         driver.get("//tumblr.com");
-        driver.close();
+        driver.getCurrentUrl();
+        driver.getPageSource();
+
+        //close the current window, will close the browser if it's the last window available
+        // driver.close();
+
+        //closes *the browser*
+        driver.quit();
 
       
         
